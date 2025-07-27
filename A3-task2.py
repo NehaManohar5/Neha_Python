0a24f34c64c7163f9788d77bb7294ad2e748c92c
NehaManohar5 <nehamcta@gmail.com>
2025-07-27 11:19:56 +0530

    Add files via upload

---
+++ A3-task2.py
@@ -0,0 +1,10 @@
+import math
+
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+logarithm = math.log(number)
+sine_value = math.sin(number)
+print("Square root:", square_root)
+print("Logarithm:", logarithm)
+print("Sine:", sine_value)
